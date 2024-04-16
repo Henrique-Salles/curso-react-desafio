b675dd12e26f48929970013d9939abfc900b0c35
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-16 12:48:20 -0300

    functionalities finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import "./App.css";
 function App() {
   const [inputName, setInputName] = useState("");
   const [inputYear, setInputYear] = useState("");
-  const [calc, setCalc] = useState(0);
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState("");
 
   function Calc() {
-    let calc = 2024 - Number(inputYear);
-    setCalc(calc);
+    let age = 2024 - Number(inputYear);
+    setAge(age);
+    let name = inputName;
+    setName(name);
   }
 
   return (
@@ -35,10 +38,9 @@ function App() {
         value="Descobrir idade"
         onClick={Calc}
       />
-
-      {calc !== 0 && (
+      {age !== 0 && (
         <h3 className="result">
-          Olá {inputName}, você tem {calc} anos
+          Olá {name}, você tem {age} anos
         </h3>
       )}
     </div>
